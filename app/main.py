12f2ad76cd5351b83a419c7593036cf70bde8547
maciejbaba <maciek2baba2@gmail.com>
2024-05-01 18:56:37 +0200

    test

--- app/main.py
+++ app/main.py
@@ -1,11 +1,19 @@
 import subprocess
 import sys
+from tempfile import mkdtemp
 
+import os
+import shutil
 
 def main():
     command = sys.argv[3]
     args = sys.argv[4:]
    
+    temporary_dir = mkdtemp()
+    shutil.copy2(command, temporary_dir)
+    os.chroot(temporary_dir)
+    command = os.path.join("/", os.path.basename(command))
+
     completed_process = subprocess.run([command, *args], capture_output=True)
 
     sys.stdout.buffer.write(completed_process.stdout)

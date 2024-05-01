bb48af07cec6ca8670ab36d62618e39f2bf6d430
maciejbaba <maciek2baba2@gmail.com>
2024-05-01 18:37:07 +0200

    test

--- app/main.py
+++ app/main.py
@@ -14,6 +14,8 @@ def main():
     completed_process = subprocess.run([command, *args], capture_output=True)
     print(completed_process.stdout.decode("utf-8"))
 
+    sys.stdout.buffer.write(completed_process.stdout)
+    sys.stderr.buffer.write(completed_process.stderr)
 
 if __name__ == "__main__":
     main()
